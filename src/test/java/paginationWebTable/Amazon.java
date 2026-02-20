096c19cd451945e4796e5ec8f4aae4b2e4627817
shilpavenkatesh111-art <shilpavenkatesh111@gmail.com>
2026-02-20 16:25:15 +0530

    conflict2 testing

--- src/test/java/paginationWebTable/Amazon.java
+++ src/test/java/paginationWebTable/Amazon.java
@@ -3,6 +3,8 @@ package paginationWebTable;
 public class Amazon {
 public static void main(String[] args) {
 	System.out.println("hi");
+	System.out.println("hello");
+	System.out.println("merger conflict2");
 }
 }
 
